c8c28105a6b3d0966d838e4f55d0ba2f3649d796
Michael Erdenberger <merdenbe@nd.edu>
2018-09-30 23:10:29 -0400

    further heroku debugging

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import mysql.connector
+import os
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
@@ -50,4 +51,5 @@ def constrainTable():
     return json.dumps(response)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
